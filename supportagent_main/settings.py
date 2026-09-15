cc2e560fb1e69ed5a86f259e19179eb797ef9aa3
shibith-dev <shibith.p.dev.com>
2026-09-15 19:00:07 +0530

    fix CSRF verification failure

--- supportagent_main/settings.py
+++ supportagent_main/settings.py
@@ -139,3 +139,7 @@ ANTHROPIC_API_KEY = config('ANTHROPIC_API_KEY')
 ANTHROPIC_MODEL = config('ANTHROPIC_MODEL')
 
 STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
+
+CSRF_TRUSTED_ORIGINS = {
+    "https://django-multiagent-support-production.up.railway.app"
+}
